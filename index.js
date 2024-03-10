a88c2a1577f838e9da5764432b49ceb66bbefff7
Tisha Gupta <tishaguptatop@gmail.com>
2024-03-10 21:19:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("code with Tisha");
+
+require('dotenv').config()
+
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter",(req,res)=>{
+  res.send('tishycs')
+}) 
+
+app.get("/login",(req,res)=>{
+  res.send('<h1>please login to twitter</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+}); 
